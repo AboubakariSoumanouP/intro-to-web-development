c452e40ef825961fffa8cb2580163432cdf300ba
AboubakariSoumanouP <soumabakari@gmail.com>
2020-03-15 12:36:34 -0500

    Competed

--- hw2/src/components/Form.js
+++ hw2/src/components/Form.js
@@ -1,40 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        
-        /*
-            TODO - set initial state for link name and URL 
-
+  constructor(props) {
+    super(props);
+    /*
+            TODO - set initial state for link name and URL
         */
-    }
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.handleChangeURL = this.handleChangeURL.bind(this);
+    this.state = { name: "", url: "" };
+  }
 
-    handleChange = event => {
-        /*
-            TODO - Logic for changing state based on form changes
-        */
-    }
+  handleChangeName = event => {
+    //TODO - Logic for changing state based on form changes
 
-    onFormSubmit = (event) => {
-        // to prevent page reload on form submit
-        event.preventDefault();
-        
-        /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
+    this.setState({
+      name: event.target.value
+    });
+  };
 
-    }
+  handleChangeURL = event => {
+    // TODO - Logic for changing state based on form changes
 
-    render() {
+    this.setState({
+      url: event.target.value
+    });
+  };
 
-        return(
-            <form>
-                {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
-            </form>
-        )
-    
-    }
+  onFormSubmit = event => {
+    // TODO - Logic for calling props to handle submission and setting state changes
+    console.log(this.state.name);
+    console.log(this.state.url);
+    event.preventDefault();
+    // this.props.newData(this.state.name);
+    this.props.handleSubmit(this.state);
+
+    // * This will reset the form on submission
+    this.setState({
+      name: "",
+      url: ""
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={this.handleChangeName}
+        />
+        <label>URL</label>
+        <input
+          type="text"
+          value={this.state.url}
+          onChange={this.handleChangeURL}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 }
 
 export default Form;
