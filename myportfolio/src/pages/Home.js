e5fc88e0a0639e1970cc1f1749cd14c6f88e8113
AboubakariSoumanouP <soumabakari@gmail.com>
2020-04-09 16:18:10 -0400

    styled home page

--- myportfolio/src/pages/Home.js
+++ myportfolio/src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import  HomeImage from '../me.jpeg';
 
 function Home(props) {
     return (
-        <h1>Home</h1>
+       <div className="home-component"> 
+           <div className="image-container">
+               <img src={HomeImage} alt=""/>
+           </div>
+           <h1>Aboubakari Soumanou Petoni</h1>
+       </div>
     )
 }
 
