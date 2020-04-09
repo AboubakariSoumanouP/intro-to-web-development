a27aafdb5b4774526c199bb1790b36e1ea8688b8
AboubakariSoumanouP <soumabakari@gmail.com>
2020-04-09 15:45:54 -0400

    set up react-router

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Route,  Switch} from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
 
 class App extends React.Component {
    
   render() {
     return (
-      <h1>Hello Word</h1>
+     <Router>
+       <NavBar />
+       <Switch>
+         <Route exact path='/' component={Home} />
+         <Route exact path='/aboutMe' component={About} />
+         <Route exact path='/projects' component={Project} />
+         <Route exact path='/contactMe' component={Contact} />
+       </Switch>
+     </Router>
     )
   }
 } 
--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function NavBar(props) {
     return (
-        <h1>NavBar</h1>
+       <ul>
+           <li><Link to='/'>Home</Link></li>
+           <li><Link to='/aboutMe'>About Me</Link></li>
+           <li><Link to='/projects'>Projects</Link></li>
+           <li><Link to='/contactMe'>Contact Me</Link></li>
+       </ul>
     )
 }
 
